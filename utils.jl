3d6cb613c27d3d208cb9d7fcd195e3aa7553a0fa
qiyang_island <qiyang_island@outlook.com>
2019-05-10 23:11:53 +0800

    debug finish ver 0.1

--- utils.jl
+++
@@ -1,9 +0,0 @@
-function string_to_array(s::String)
-    temp_array = Array{Char}(UndefInitializer(),length(s))
-    for i in 1:1:length(s)
-        temp_array[i] = s[i]
-    end
-    return parse.(Type,temp_array)
-end
-
-
